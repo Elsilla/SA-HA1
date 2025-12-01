d5058af6dbc6775ac132025b1964c80bb4538e1c
Tiago Alcázar <tiagofabian195@outlook.com>
2025-12-01 17:18:48 -0300

    new section thematic-collections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import HeroSlider from '../components/HeroSlider'
 import Testimonials from '../components/Testimonials'
+import Carousel from "@/components/Carousel";
+import ThematicCollections from '@/components/ThematicCollections';
 import Benefits from '../components/Benefits'
 import { carousel } from "@/lib/carousel";
-import Carousel from "@/components/Carousel";
 
 const Home = () => {
   return (
     <div className='container-home'>
       <HeroSlider/>
       <Carousel products={carousel}/>
+      {/* <ThematicCollections/> */}
       <Testimonials/>
       <Benefits/>
     </div>
